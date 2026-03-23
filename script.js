d72a548c053ef2ab7d976bbf354e92f40517904c
williamtaipe2 <williamtaipe2@gmail.com>
2026-03-22 22:52:27 -0500

    Update WhatsApp link in button click event

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const botones = document.querySelectorAll('.btn');
 botones.forEach(btn => {
     btn.addEventListener('click', (e)=>{
         e.preventDefault();
-        window.open("https://wa.me/51999999999?text=Hola%20quiero%20comprar%20un%20polo%20de%20Authentic%20Streetwear");
+        window.open("https://wa.me/51912963421?text=Hola%20quiero%20comprar%20un%20polo%20de%20Authentic%20Streetwear");
     });
 });
